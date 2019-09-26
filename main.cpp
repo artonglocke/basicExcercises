daf18e4bb1f520ce588c25d04c327232b5e184e4
visualstudiomachina <visualstudiomachina@outlook.com>
2019-09-26 20:52:39 +0200

    Zadatak 3.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,39 @@
 ﻿#include <iostream>
 #include <string>
 
-/*
-napisati prvi broj koji je djeljiv sa svim brojevima u intervalu od 0-10
-*/
-
-/*
-		nadopuniti program tako da ovo radi : 
-		int main()
-		{
-			std::coun<< number++ << std::endl;
-			std::cout<<++number<<std::endl;
-		}
-
-*/
+		/*nadopuniti program tako da ovo radi :*/
+class number
+{
+public:
+	number(int i):
+		mNumber(i)
+	{}
+	number operator++(int)
+	{
+		int newNumber = mNumber;
+		++mNumber;
+		return newNumber;
+	}
+	number operator++()
+	{
+		++mNumber;
+		return mNumber;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const number& n);
+private:
+	int mNumber;
+};
+std::ostream& operator<<(std::ostream& outputStream, const number& n)
+{
+	return outputStream <<"["<< n.mNumber<<"]"<<std::endl;
+}
 int main()
 {
-	int i = 12;
-	for ( ; ; ++i)
-	{
-		bool ivanovaCigareta = true;
-		for (int j = 1; j <= 4; ++j)
-		{
-			if (i % j != 0)
-			{
-				ivanovaCigareta = false;
-				break;
-			}
-		}
-		if (ivanovaCigareta == true)
-		{
-			std::cout << i << std::endl;
-			break;
-		}
-	}
+	number num(2);
+
+
+	std::cout << num++ << std::endl;
+	std::cout << ++num << std::endl;
+
 }
+
