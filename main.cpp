ff3afb4df87a72e6ab8cc5ea31c6cafdde83624c
visualstudiomachina <visualstudiomachina@outlook.com>
2019-09-26 20:09:53 +0200

    Zadatak 2

--- main.cpp
+++ main.cpp
@@ -1,32 +1,37 @@
 ﻿#include <iostream>
 #include <string>
-/*napisati funkciju koja primi jedan pozitivan broj,
-u slucaju da je broj djeljiv sa 3 neka vrati fizz
-u slucaju da je broj djeljiv sa 5 neka vrati buzz
-u slucaju da je broj djeljiv sa 3 i pet neka vrati fizz-buzz
-u slucaju da nije djeljiv sa nijednim neka vrati " " (prazan string)*/
 
+/*
+napisati prvi broj koji je djeljiv sa svim brojevima u intervalu od 0-10
+*/
 
-std::string myFoo(const int& i)
-{
-	if (i % 3 == 0)
-	{
-		if (i % 5 == 0)
+/*
+		nadopuniti program tako da ovo radi : 
+		int main()
 		{
-			return "fizz-buzz";
+			std::coun<< number++ << std::endl;
+			std::cout<<++number<<std::endl;
 		}
-		return "fizz";
-	}
-	if (i % 5 == 0)
-		return "buzz";
 
-	return " ";
-
-}
+*/
 int main()
 {
-	int i = 0;
-	std::cin >> i;
-	std::cout << myFoo(i) << std::endl;
-
+	int i = 12;
+	for ( ; ; ++i)
+	{
+		bool ivanovaCigareta = true;
+		for (int j = 1; j <= 4; ++j)
+		{
+			if (i % j != 0)
+			{
+				ivanovaCigareta = false;
+				break;
+			}
+		}
+		if (ivanovaCigareta == true)
+		{
+			std::cout << i << std::endl;
+			break;
+		}
+	}
 }
